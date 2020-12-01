ab7f9c174db7ae488a1eb98314454b55720d11ef
Abdulah Omran <abdulah.omran@patelhs.org>
2020-12-01 16:50:51 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+String[] roster1 = {"James", "Dylan","Austin", "Aaron", "Luke", "Erin", "Michael","Andy","Jim","Dwight"};
+String[] roster2 = new String [10];
+
+for(int i = 0; i <= 9; i++){
+roster2[i] = roster1[i];
+System.out.println(roster2[i] + "");
+} 
+
+  }
+}
